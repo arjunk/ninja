6223cf4cead56019ded8683418209b120d6af4f2
Arjun Khandelwal <aakhandelwal@gmail.com>
2012-10-25 12:44:05 +0530

    layout of first screen

--- TechRadar/gen/com/tw/techradar/R.java
+++ TechRadar/gen/com/tw/techradar/R.java
@@ -11,11 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int about_radar_button=0x7f020000;
+        public static final int about_us_button=0x7f020001;
+        public static final int background=0x7f020002;
+        public static final int circle=0x7f020003;
+        public static final int current_radar_button=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int references_button=0x7f020007;
+        public static final int user_guide_button=0x7f020008;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int col1=0x7f070004;
+        public static final int col2=0x7f070005;
+        public static final int col3=0x7f070006;
+        public static final int menu_settings=0x7f070009;
+        public static final int row1=0x7f070000;
+        public static final int row2=0x7f070001;
+        public static final int row3=0x7f070002;
+        public static final int row4=0x7f070003;
+        public static final int row5=0x7f070007;
+        public static final int row6=0x7f070008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,10 +40,16 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int about_radar=0x7f040008;
+        public static final int about_us=0x7f040004;
         public static final int app_name=0x7f040000;
+        public static final int current_Radar=0x7f040009;
+        public static final int current_radar=0x7f040005;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int references=0x7f040006;
         public static final int title_activity_main=0x7f040003;
+        public static final int user_guide=0x7f040007;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
