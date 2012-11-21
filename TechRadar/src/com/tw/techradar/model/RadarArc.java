e6858b7e6e2f1f52d803ca84176961b9e76a6809
Pawan Shah <pawanvshah@yahoo.com>
2012-11-21 14:41:42 +0530

    Pawan : Using Jackson to parse entire Radar Data from json

--- TechRadar/src/com/tw/techradar/model/RadarArc.java
+++ TechRadar/src/com/tw/techradar/model/RadarArc.java
@@ -1,31 +1,40 @@
 package com.tw.techradar.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RadarArc implements Serializable{
-    private int radius;
+    private int r;
     private String name;
     private int startOffset;
 
-    public RadarArc(int radius, String name, int startOffset) {
-        this.radius = radius;
-        this.name = name;
-        this.startOffset = startOffset;
+    public int getStartOffset() {
+        return startOffset;
     }
 
-    public int getRadius() {
-        return radius;
+    public int setStartOffset(int offset) {
+        return this.startOffset = offset;
+    }
+
+    public boolean isRadarItemInArc(RadarItem radarItem){
+        return (radarItem.getRadius() >= this.startOffset) && (radarItem.getRadius() <= this.r);
     }
 
     public String getName() {
         return name;
     }
 
-    public int getStartOffset() {
-        return startOffset;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public boolean isRadarItemInArc(RadarItem radarItem){
-        return (radarItem.getRadius() >= this.startOffset) && (radarItem.getRadius() <= this.radius);
+    public int getRadius() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
     }
 }
